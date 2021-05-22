be9a1f3f2ca4d9b4524e80bd0567586de0c49558
wfercanas <fernando.canas@outlook.com>
2021-05-22 15:00:54 -0500

    add/ new starting posts

--- src/livechat/livechat-data.js
+++ src/livechat/livechat-data.js
@@ -19,6 +19,21 @@ const livechatContentData = [
     avatarURL: '../../assets/avatars/Arjun.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
+  {
+    username: 'Beepboop',
+    avatarURL: '../../assets/avatars/Beepboop.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
+  },
+  {
+    username: 'Blair Randall',
+    avatarURL: '../../assets/avatars/Blair.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
+  },
+  {
+    username: 'Claudia Toro',
+    avatarURL: '../../assets/avatars/Claudia.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
+  },
 ];
 
 const livechatUserData = {
