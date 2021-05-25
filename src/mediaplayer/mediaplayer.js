f9e8efa5d5e4ac973c812141687bd7aa59fe13db
wfercanas <fernando.canas@outlook.com>
2021-05-25 15:12:21 -0500

    add/ volume and mute buttons controlled

--- src/mediaplayer/mediaplayer.js
+++ src/mediaplayer/mediaplayer.js
@@ -9,6 +9,9 @@ const currentMin = document.getElementById('current-min');
 const currentSec = document.getElementById('current-sec');
 const durationMin = document.getElementById('duration-min');
 const durationSec = document.getElementById('duration-sec');
+const volumeButton = document.getElementById('volume-button');
+const muteButton = document.getElementById('mute-button');
+const volumeRange = document.getElementById('volume-range');
 
 const totalVideos = videoSources.length;
 let currentIndexVideo = 0;
@@ -81,8 +84,42 @@ const setNewVideo = () => {
 };
 
 /* Control volume */
-const setVolume = (event) => {
-  video.volume = event.target.value / 100;
+const setInitialVolume = () => {
+  video.volume = 0.5;
+  volumeRange.value = 50;
+};
+
+const setVolume = () => {
+  video.volume = volumeRange.value / 100;
+  if (video.volume > 0) {
+    turnOnVolumeButton();
+  } else {
+    turnOnMuteButton();
+  }
+};
+
+const toggleMute = (event) => {
+  if (event.target.id === 'volume-button' && !video.muted) {
+    volumeButton.classList.add('off');
+    muteButton.classList.remove('off');
+    video.volume = 0;
+    volumeRange.value = 0;
+  } else {
+    volumeButton.classList.remove('off');
+    muteButton.classList.add('off');
+    video.volume = 0.5;
+    volumeRange.value = 50;
+  }
+};
+
+const turnOnVolumeButton = () => {
+  volumeButton.classList.remove('off');
+  muteButton.classList.add('off');
+};
+
+const turnOnMuteButton = () => {
+  volumeButton.classList.add('off');
+  muteButton.classList.remove('off');
 };
 
 /* Control widescreen */
@@ -125,6 +162,12 @@ controls.addEventListener('click', (event) => {
       startVideoProgress();
       setTimeout(setVideoDuration, 1000);
       break;
+    case 'volume-button':
+      toggleMute(event);
+      break;
+    case 'mute-button':
+      toggleMute(event);
+      break;
     case 'volume-range':
       setVolume(event);
       break;
@@ -133,3 +176,5 @@ controls.addEventListener('click', (event) => {
       break;
   }
 });
+
+window.addEventListener('load', setInitialVolume);
