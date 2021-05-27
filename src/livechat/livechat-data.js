b547c7cf216790bdcb17faca9b5b9e00a7e29502
wfercanas <fernando.canas@outlook.com>
2021-05-27 15:40:57 -0500

    fix/ avatar paths adjusted

--- src/livechat/livechat-data.js
+++ src/livechat/livechat-data.js
@@ -1,37 +1,37 @@
 const livechatContentData = [
   {
     username: 'Aliza Abadi',
-    avatarURL: './assets/avatars/Aliza.svg',
+    avatarURL: '../../assets/avatars/Aliza.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
   {
     username: 'Alvin Wise',
-    avatarURL: './assets/avatars/Alvin.svg',
+    avatarURL: '../../assets/avatars/Alvin.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
   {
     username: 'Angie Hakim',
-    avatarURL: './assets/avatars/Angie.svg',
+    avatarURL: '../../assets/avatars/Angie.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
   {
     username: 'Arjun Hope',
-    avatarURL: './assets/avatars/Arjun.svg',
+    avatarURL: '../../assets/avatars/Arjun.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
   {
     username: 'Beepboop',
-    avatarURL: './assets/avatars/Beepboop.svg',
+    avatarURL: '../../assets/avatars/Beepboop.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
   {
     username: 'Blair Randall',
-    avatarURL: './assets/avatars/Blair.svg',
+    avatarURL: '../../assets/avatars/Blair.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
   {
     username: 'Claudia Toro',
-    avatarURL: './assets/avatars/Claudia.svg',
+    avatarURL: '../../assets/avatars/Claudia.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, nesciunt! Beatae, maxime molestiae.',
   },
 ];
